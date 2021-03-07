7a6312c18ff7b5aafecf76bf5d1c5520961fdc3b
Philip Hildebrandt <Philiphi@web.de>
2021-03-07 16:14:50 +0100

    [added] shelf script

---
+++ LibraryLadder/Assets/scripts/Shelf.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shelf : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("du bist in meiner triggerzone");
+        if(collider.TryGetComponent(out PlayerMovement player))
+        {
+            player.possibleShelfs.Add(this);
+        }
+        else
+        {
+            Debug.Log("aber hast kein playermovement script");
+        }
+    } 
+
+}
